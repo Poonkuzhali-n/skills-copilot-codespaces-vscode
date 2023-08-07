d0ec59284954e2f4ac8e5467dedc59a70b4c5f2d
Poonkuzhali-n <140244262+Poonkuzhali-n@users.noreply.github.com>
2023-08-07 07:17:58 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember(){
+    var member = {
+        name: "John Doe",
+        age: 30,
+        address: "123 Main St",
+        calculateNumbers: function(var1,var2){
+            return var1 + var2;
+        }
+    };
+    console.log(member.calculateNumbers(1,2));
+}
